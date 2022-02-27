27ca907f78b263dc6e02b0ccb6074593acf10c05
Gabriel Nogueira <gab.nog94@gmail.com>
2022-02-26 22:34:07 -0300

    [Trabalho 2] Fix main

--- Trabalho-2/main.cpp
+++ Trabalho-2/main.cpp
@@ -8,15 +8,11 @@ using namespace std;
 
 
 int main() {
+
     init();
     load_mem("code.bin", TEXT_SEGMENT_START);
     load_mem("data.bin", DATA_SEGMENT_START);
-    // dump_memory(DATA_SEGMENT_START, DATA_SEGMENT_START + 4*20);
-    // run(true, true);
     run();
-    // dump_reg('h');
-    
-
   
     return 0;
 }

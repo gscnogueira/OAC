e26eb07381d372dc1cd18b814a7048773cb7970b
Gabriel Nogueira <gab.nog94@gmail.com>
2022-03-18 23:13:42 -0300

    [Trabalho 3] Refatoração da função `plotm`

--- Trabalho-3/jogo_da_vida.asm
+++ Trabalho-3/jogo_da_vida.asm
@@ -21,6 +21,7 @@ mat2: .byte 0:255
 heap: .word 0x00003000
 display_size: .word 0x400
 cor: .word 0x008be9fd
+
 .text
 
 la a0, mat1
@@ -33,12 +34,12 @@ j exit
 
 #-------------------------------------
 readm:
-	slli t5, a0, 4
+	slli t0, a0, 4
 	# obter posição relativa à linha:
-	add t5,t5, a1  
+	add t0,t0, a1  
 	# obter posição relativa à coluna:
-	add t5, t5, a2 
-	lb a0, 0(t5)
+	add t0, t0, a2 
+	lb a0, 0(t0)
 	ret
 #-------------------------------------
 plotm:
@@ -46,12 +47,12 @@ plotm:
 	lw s1, cor  	# carrega cor
 	li s2, 0x10 # altura da matriz
 	# Inicializa contador
-	mv t3, zero
+	mv s3, zero
 loopi:
-	bge t3, s2, endi
-	mv t4, zero
+	bge s3, s2, endi
+	mv s4, zero
 loopj:
-	bge t4, s2 endj
+	bge s4, s2 endj
 	addi sp, sp, -8
 	sw a0, 0(sp)
 	sw ra, 4(sp)
@@ -60,29 +61,28 @@ loopj:
 	lw ra, 4(sp)
 	addi sp, sp 8
 	addi s0, s0, 4
-	addi t4, t4, 1
+	addi s4, s4, 1
 	j loopj
 endi:
 	ret
 endj:
-	addi t3, t3, 1
+	addi s3, s3, 1
 	j loopi	
 
 pinta_pixel:
 	addi sp, sp, -4
 	sw ra, 0(sp)
 	mv a2, a0
-	mv a1, t4
-	mv a0, t3
+	mv a1, s4
+	mv a0, s3
 	call readm
 	lw ra, 0(sp)
 	addi sp, sp, 4
 	beqz a0, ppend
 	sw s1, 0(s0)
 ppend:	ret
-	
-pFIM: ret	
-	
+		
+#-------------------------------------	
 exit:
 	li a7, 10
 	ecall

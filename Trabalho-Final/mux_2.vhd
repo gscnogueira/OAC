cf8066ce34a536719139d14ee7c746da48186e6e
Gabriel Nogueira <gab.nog94@gmail.com>
2022-05-06 19:23:02 -0300

    [Trabalho Final] Arruma identação

--- Trabalho-Final/mux_2.vhd
+++ Trabalho-Final/mux_2.vhd
@@ -1,13 +1,17 @@
-component mux_2 is
-    generic (WSIZE : natural := 32);
-    port (
-      sel      : in  std_logic;
-      in0, in1 : in  std_logic_vector(WSIZE-1 downto 0);
-      m_out    : out std_logic_vector(WSIZE-1 downto 0)
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mux_2 is
+  generic (WSIZE : natural := 32);
+  port (
+    sel      : in  std_logic;
+    in0, in1 : in  std_logic_vector(WSIZE-1 downto 0);
+    m_out    : out std_logic_vector(WSIZE-1 downto 0)
     );
-end component;
+end entity;
 
 architecture rtl of mux_2 is
 begin
-    m_out <= in0 when (sel = '0') else in1;
+  m_out <= in0 when (sel = '0') else in1;
 end rtl;

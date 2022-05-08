f62b47e4c0f58f6d2dc7081c8e509f18bc927d06
Gabriel Nogueira <gab.nog94@gmail.com>
2022-05-08 19:05:52 -0300

    [Trabalho Final] Fix mux_lui

--- Trabalho-Final/mux_lui.vhd
+++ Trabalho-Final/mux_lui.vhd
@@ -13,13 +13,12 @@ end mux_lui_auipc;
 architecture rtl of mux_lui_auipc is
   signal shifted_imm : std_logic_vector(31 downto 0);
 begin
-  shifted_imm <= imm(19 downto 0) & X"000";
   process (lui, auipc, imm, pc, data)
   begin
     if(lui = '1') then
-      m_out <= shifted_imm;
+      m_out <= imm;
     elsif(auipc = '1')then
-      m_out <= std_logic_vector(unsigned(shifted_imm) + unsigned(pc));
+      m_out <= std_logic_vector(unsigned(imm) + unsigned(pc));
     else
       m_out <= data;
     end if;
